015c6959b88e1f21014186f1912f96a2508252c6
renzo <renzosy1357@gmail.com>
2025-12-28 20:28:21 +0100

    actualizacion0.10

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // Products
-export * from "./dto/products/Product.dto";
-export * from "./dto/products/update-product.dto";
+export * from "./dto/products/Product.dto.js";
+export * from "./dto/products/update-product.dto.js";
 
 // Orders
-export * from "./dto/orders/create-order.dto";
+export * from "./dto/orders/create-order.dto.js";
+
+/*
+    Al poner "verbatimModuleSyntax": false, desabilito las extensiones de ".js",
+    sin embargo, nodejs (de mis projectos), usa ESM(ECMAScript), que requiere explicitamente
+    que se utilizen las extensiones al hacer los importes
+*/
