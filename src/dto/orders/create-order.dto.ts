bea6d36ae523bc3dee5606519f6ea9cd346d1bf6
renzo <renzosy1357@gmail.com>
2025-12-30 18:55:37 +0100

    update06

--- src/dto/orders/create-order.dto.ts
+++ src/dto/orders/create-order.dto.ts
@@ -1,6 +1,6 @@
 import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
-import { OrdenItemDto } from "./order-item.dto";
+import { OrdenItemDto } from "./order-item.dto.js";
 
 export class CreateOrderDto {
   @IsArray()
